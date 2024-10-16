5c622df93489e047918bf570c14a1d42d96664e7
Anish Sevekari <anish.sevekari@gmail.com>
2024-10-16 05:52:01 +0000

    adding formatter

--- flake.nix
+++ flake.nix
@@ -7,33 +7,51 @@
     trusted-public-keys = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E= hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=";
   };
 
-	inputs = {
-		# Tie inputs to ones defined in nixos configuration
-		nixos-config.url = "github:andyseve/nixos";
-		nixpkgs.follows = "nixos-config/nixpkgs";
-		unstable.follows = "nixos-config/unstable";
+  inputs = {
+    # Tie inputs to ones defined in nixos configuration
+    nixos-config.url = "github:andyseve/nixos";
+    nixpkgs.follows = "nixos-config/nixpkgs";
+    unstable.follows = "nixos-config/unstable";
+    treefmt-nix.follows = "nixos-config/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
 
     # Manages configs and home directory
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs :
+  outputs =
+    inputs:
     let
-      system = "x86_64-linux";
+      mkHost = host: hostConfig: {
+        name = "stranger@${host}";
+        value = inputs.home-manager.lib.homeManagerConfiguration {
+          system = hostConfig.system;
           pkgs = import inputs.nixpkgs {
-	  	inherit system;
+            system = hostConfig.system;
             config.allowUnfree = hostConfig.unfree;
           };
+          extraSpecialArgs = {
             upkgs = import inputs.unstable {
-	    inherit system;
+              system = hostConfig.system;
               config.allowUnfree = hostConfig.unfree;
             };
-    in rec {
-      homeConfigurations."stranger" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-	extraSpecialArgs = {};
-        modules = nixos-config.utils.listModules' (toString ./modules);
+          };
+          modules = inputs.nixos-config.utils.listModules' (toString ./modules);
+        };
       };
+    in
+    rec {
+      hostnames = inputs.nixos-config.hostnames;
+      homeConfigurations = inputs.nixpkgs.lib.mapAttrs' mkHost inputs.nixos-config.hostConfigs;
+      formatter = builtins.listToAttrs (
+        builtins.map (system: {
+          name = system;
+          value =
+            (inputs.treefmt-nix.lib.evalModule (inputs.nixpkgs.legacyPackages.${system}) ./treefmt.nix)
+            .config.build.wrapper;
+          # value = inputs.nixpkgs.legacyPackages.${system}.treefmt;
+        }) inputs.nixos-config.systems
+      );
     };
 }
