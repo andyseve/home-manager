741be691cd2e30b65c7d0f9f85056dfc1bf86aa4
Anish Sevekari <anish.sevekari@gmail.com>
2024-12-27 17:47:48 -0500

    cleanup and minor updates

--- flake.nix
+++ flake.nix
@@ -1,11 +1,5 @@
 {
-  description = "Home Manager configuration of stranger";
-
-  nixConfig = {
-    extra-experimental-features = "nix-command flakes";
-    extra-substituters = "https://cache.nixos.org https://nix-community.cachix.org https://cuda-maintainers.cachix.org https://hyprland.cachix.org";
-    trusted-public-keys = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E= hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=";
-  };
+  description = "Home Manager configuration for Anish Sevekari";
 
   inputs = {
     # Tie inputs to ones defined in nixos configuration
@@ -43,13 +37,6 @@
     rec {
       hostnames = inputs.nixos-config.hostnames;
       homeConfigurations = inputs.nixpkgs.lib.mapAttrs' mkHost inputs.nixos-config.hostConfigs;
-      formatter = builtins.listToAttrs (
-        builtins.map (system: {
-          name = system;
-          value =
-            (inputs.treefmt-nix.lib.evalModule (inputs.nixpkgs.legacyPackages.${system}) ./treefmt.nix)
-            .config.build.wrapper;
-        }) inputs.nixos-config.systems
-      );
+      formatter = inputs.nixos-config.formatter;
     };
 }
--- modules/vscode/vscode.nix
+++ modules/vscode/vscode.nix
@@ -1,10 +1,8 @@
-
-{ pkgs, upkgs, ...}: {
+{ pkgs, ... }:
+{
   # structure - add extensions separately and configure the .vscode dir
   # through home manager. It would be best if there is some way to
   # automatically create vscode dirs per project that the global config since
   # the global config does nothing but clutter data.
-	home.packages = [
-	    pkgs.vscode
-	];
+  home.packages = [ pkgs.vscode ];
 }
